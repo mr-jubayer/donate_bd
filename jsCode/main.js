6aca9cb5cb23bed3e9407dbf03b0f30b1ff0b992
jubayer <jubayerahmed46@gmail.com>
2024-09-23 17:45:24 +0600

    functionality added

--- jsCode/main.js
+++ jsCode/main.js
@@ -35,3 +35,87 @@ function showCurrentDatas(activeBtn, deActivebtn, showConten, hideContent) {
   hideContent.classList.add("hidden");
   showConten.classList.remove("hidden");
 }
+
+// donate people
+const userBalance = selectElById("user-balence");
+const userMobileBalance = selectElById("user-m-balence");
+
+const noakhaliDonateBtn = selectElById("noakhali-donatingBtn");
+
+// donate for noakhali
+noakhaliDonateBtn.addEventListener("click", function () {
+  const noakhaliOrganizeAmt = selectElById("noakhaliDonatedValue");
+  const noakhaliOrgTitle = selectElById("n-title");
+  const inputDonate = selectElById("noakhali-inputDonate");
+  const modalId = my_modal_1;
+  donatePeople(noakhaliOrganizeAmt, noakhaliOrgTitle, inputDonate, modalId);
+});
+// doante fo feni
+const feniDonateBtn = selectElById("feni-donatingBtn");
+feniDonateBtn.addEventListener("click", function () {
+  const feniOrgAmt = selectElById("feniOrgAmt");
+  const feniOrgTitle = selectElById("f-title");
+  const inputDonate = selectElById("feni-inputDonate");
+  const modalId = my_modal_2;
+  donatePeople(feniOrgAmt, feniOrgTitle, inputDonate, modalId);
+});
+// doante fo qouta injured people
+const qoutaInjuredDonateBtn = selectElById("qouta-donatingBtn");
+qoutaInjuredDonateBtn.addEventListener("click", function () {
+  const qoutaInjuredOrganizeAmt = selectElById("qoutaOrgAmt");
+  const qoutaOrgTitle = selectElById("q-title");
+  const inputDonate = selectElById("qouta-inputDonate");
+  const modalId = my_modal_3;
+  donatePeople(qoutaInjuredOrganizeAmt, qoutaOrgTitle, inputDonate, modalId);
+});
+
+function donatePeople(orgAmt, orgTitle, inpDon, modalId) {
+  let parseUserBal = parseFloat(userBalance.innerText);
+  let inputDonate = parseFloat(inpDon.value);
+  // donaton validation
+  if (isNaN(inputDonate) || inputDonate <= 0) {
+    alert("amount is invalid");
+    return;
+  } else if (inputDonate > parseUserBal) {
+    alert("you  don't have enough money for donate");
+    return;
+  }
+  modalId.showModal();
+
+  // org amount
+  let organizeAmt = parseFloat(orgAmt.innerText);
+
+  // add to org amount of user amount
+  organizeAmt += inputDonate;
+  // subtruct user amount
+  parseUserBal -= inputDonate;
+  // show everything in the page
+  orgAmt.innerText = organizeAmt;
+  userBalance.innerText = parseUserBal;
+  userMobileBalance.innerText = parseUserBal;
+
+  // add history
+  let newHistory = document.createElement("div");
+  newHistory.classList.add("rounded-md", "border-[1.4px]", "p-5");
+  selectElById("emptyHis").classList.add("hidden");
+
+  let date = new Date();
+
+  newHistory.innerHTML = `
+            <h3 class="font-bold md:text-customLg mb-2 flex gap-2 text-left">
+              <p id="palce-name">
+                <span id="doanted-amount"> ${inputDonate} Taka</span>
+                ${orgTitle.innerText}
+              </p>
+            </h3>
+            <p class="text-gray-600 md:text-sm text-xs">
+              Date :
+              <span id="date"
+                >${date.toString()}
+                Time)</span
+              >
+            </p>`;
+
+  historyContainer.insertBefore(newHistory, historyContainer.firstChild);
+  inpDon.value = "";
+}
